99b9045e7e6d250e313bf67b59e028d2e2f0a30a
marc-abertay <marci.philippe@googlemail.com>
2018-03-17 11:23:40 +0000

    turret now rotating towards targets

--- Dam Good Game/Assets/Scripts/Turret.cs
+++ Dam Good Game/Assets/Scripts/Turret.cs
@@ -18,7 +18,7 @@ public class Turret : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        DetectTarget();
 	}
 
     void DetectTarget()
@@ -26,13 +26,29 @@ public class Turret : MonoBehaviour {
         // Create circle collider that finds all the objects within the radius
         Collider2D[] DetectedColliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), DetectionDistance, DetectionLayer, -Mathf.Infinity, Mathf.Infinity);
         // Check if the collider has detected a ship that is nearby
+        Debug.Log(DetectedColliders.Length);
         if (DetectedColliders.Length > 0)
         {
-            // create variable to determine what ship is closest to the turret
-            float closestDistance;
+            // Create variable to determine what ship is closest to the turret
+            float closestDistance = Mathf.Infinity;
             for (int i = 0; i < DetectedColliders.Length; i++)
             {
-
+				// Determine the distance between the collider and the turret
+				float curDistance = Vector2.Distance(transform.position, DetectedColliders[i].transform.position);
+                // Check if the current collider is closer to the previous
+                if(curDistance < closestDistance)
+                {
+                    // Set this to be the closest distance
+                    closestDistance = curDistance;
+                    // Create vector that determines the new direction our object wants to be facing
+                    Vector2 lookDirection = (DetectedColliders[i].transform.position - transform.position).normalized;
+                    // Get the angle that we need to rotate to face the object
+                    float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+                    // Convert that to quaternion for rotation
+                    Quaternion lookRotation = Quaternion.AngleAxis(lookAngle, transform.forward);
+                    // Rotate the object over time
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * RotationRate);
+                }
             }
         }
     }
